d0ba0384de5e02578e8d7b57acc575704f8d2b38
Thierry Guedj <thguedj@gmail.com>
2020-03-02 02:06:44 +0100

    Create Drawer

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <myToolbar />
+
+    <v-content>
+      <v-slide-y-transition mode="out-in">
+        <router-view />
+      </v-slide-y-transition>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+  import myToolbar from "@/components/MyToolbar";
+  export default {
+    name: "app",
+    components: { myToolbar }
+  };
+</script>
---
+++ src/components/MyToolbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-app-bar dark color="deep-purple" clipped-left app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title @click="goHome" class="white--text">{{$t("title")}}</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" clipped fixed app>
+      <myContentDrawer />
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+import myContentDrawer from "@/components/Drawer" // <== Ajouter l’import
+export default {
+  name: 'myToolbar',
+  components: {myContentDrawer}, // <== Ajouter la déclaration du composant
+  data: function () {
+    return {
+      drawer: false
+    }
+  },
+  methods: {
+    goHome () {
+      this.$router.replace('/')
+    }
+  }
+}
+</script>
